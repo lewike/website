463b233b311cd8f43b32947a835c9b32abc3e474
daidongsheng <daids@mail.com>
2022-04-26 19:59:07 +0800

    update

--- app/filters.php
+++ app/filters.php
@@ -6,15 +6,6 @@
 
 namespace App;
 
-/**
- * Add "… Continued" to the excerpt.
- *
- * @return string
- */
-add_filter('excerpt_more', function () {
-    return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), '查看更多');
-});
-
 add_filter('comment_form_default_fields', function ($fields) {
     $fields['author'] = '<p class="comment-form-author"><input id="author" name="author" type="text" value="" size="30" maxlength="245" required placeholder="你的昵称"/></p>';
     $fields['email']  = '<p class="comment-form-email"><input id="email" name="email" type="email" value="" size="30" maxlength="100" aria-describedby="email-notes" required placeholder="你的Email"/></p>';
