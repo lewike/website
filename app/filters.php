229dca38d026e6504282c343fc5f9adc927da028
daidongsheng <daids@mail.com>
2022-04-26 20:00:21 +0800

    update

--- app/filters.php
+++ app/filters.php
@@ -6,6 +6,10 @@
 
 namespace App;
 
+add_filter('excerpt_more', function () {
+    return '...';
+});
+
 add_filter('comment_form_default_fields', function ($fields) {
     $fields['author'] = '<p class="comment-form-author"><input id="author" name="author" type="text" value="" size="30" maxlength="245" required placeholder="你的昵称"/></p>';
     $fields['email']  = '<p class="comment-form-email"><input id="email" name="email" type="email" value="" size="30" maxlength="100" aria-describedby="email-notes" required placeholder="你的Email"/></p>';
